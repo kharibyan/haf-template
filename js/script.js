7cb3d423d6f603635d879e7fcf722adefc3824cd
kharibyan <>
2020-07-09 13:17:53 +0200

    + 0907201317

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
+let navbar = $('#navbarMain')
+let navbarList = $('#navbarMainList')
+let navbarInitialOffset = navbar.offset().top
+
 function justifyNavbar() {
     let wWidth = $(window).width()
 
-    // 992 is md breakpoint of bootstrap
-    if (wWidth < 992) {
-        $('#navbarMain').removeClass('sticky-top')
-        $('#navbarMain').addClass('fixed-top')
-        $('#navbarMainList').removeClass('nav-justified')
+    // 768 is sm breakpoint of bootstrap
+    if (wWidth < 768) {
+        navbar.addClass('stick-to-top')
+        navbarList.removeClass('nav-justified')
     } else {
-        $('#navbarMain').removeClass('fixed-top')
-        $('#navbarMain').addClass('sticky-top')
-        $('#navbarMainList').addClass('nav-justified')
+        if (window.pageYOffset >= navbarInitialOffset)
+            navbar.addClass('stick-to-top')
+        else
+            navbar.removeClass('stick-to-top')
+
+        navbarList.addClass('nav-justified')
     }
 }
 
 $(document).ready(justifyNavbar)
 $(window).resize(justifyNavbar)
+$(window).scroll(justifyNavbar)
+
+// TEMPORARY !!!
+function toggleBrandIcons() {
+    if ($('#brandIcons').hasClass('hidden'))
+        $('#brandIcons').removeClass('hidden')
+    else
+        $('#brandIcons').addClass('hidden')
+}
+
+console.log($('#navbarMain').height())
