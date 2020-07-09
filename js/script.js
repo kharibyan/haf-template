ec69ee836afdb0e89984e4410b71feb2493d2a02
kharibyan <>
2020-07-10 00:10:55 +0200

    + 1007200010

--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
-let navbar = $('#navbarMain')
-let navbarList = $('#navbarMainList')
-let navbarInitialOffset = navbar.offset().top
+const $navbarMain = $('#navbarMain')
+const $navbarList = $('#navbarList')
+const $navbarBrand = $('#navbarBrand')
+const navbarInitialOffset = $navbarMain.offset().top
+
+const $dropdown = $(".dropdown")
+const $dropdownToggle = $(".dropdown-toggle")
+const $dropdownMenu = $(".dropdown-menu")
+const showClass = "show"
 
 function justifyNavbar() {
     let wWidth = $(window).width()
 
     // 768 is sm breakpoint of bootstrap
     if (wWidth < 768) {
-        navbar.addClass('stick-to-top')
-        navbarList.removeClass('nav-justified')
+        $navbarMain.addClass('stick-to-top shadow-sm')
+        $navbarBrand.addClass('hidden')
+        $navbarList.removeClass('nav-justified')
+        $dropdown.removeClass('dropdown-bubble')
     } else {
-        if (window.pageYOffset >= navbarInitialOffset)
-            navbar.addClass('stick-to-top')
-        else
-            navbar.removeClass('stick-to-top')
+        if (window.pageYOffset >= navbarInitialOffset) {
+            $navbarMain.addClass('stick-to-top shadow-sm')
+            $navbarBrand.removeClass('hidden')
+        } else {
+            $navbarMain.removeClass('stick-to-top shadow-sm')
+            $navbarBrand.addClass('hidden')
+        }
 
-        navbarList.addClass('nav-justified')
+        $dropdown.addClass('dropdown-bubble')
+        $navbarList.addClass('nav-justified')
     }
 }
 
-$(document).ready(justifyNavbar)
+$(document).ready(function () {
+    justifyNavbar()
+    console.log(jQuery._data($dropdown[0], "events"))
+    console.log(jQuery._data($dropdownToggle[0], "events"))
+    console.log(jQuery._data($dropdownMenu[0], "events"))
+})
 $(window).resize(justifyNavbar)
 $(window).scroll(justifyNavbar)
+
+$(window).on('load resize', function () {
+    if (this.matchMedia('(min-width: 768px)').matches) {
+        $dropdown.hover(
+            function () {
+                const $this = $(this)
+                $this.addClass(showClass)
+                $this.find($dropdownToggle).attr("aria-expanded", "true")
+                $this.find($dropdownMenu).addClass(showClass)
+            },
+            function () {
+                const $this = $(this)
+                $this.removeClass(showClass)
+                $this.find($dropdownToggle).attr("aria-expanded", "false")
+                $this.find($dropdownMenu).removeClass(showClass)
+            }
+        )
+    } else {
+        $dropdown.off("mouseenter mouseleave");
+    }
+})
 
 // TEMPORARY !!!
 function toggleBrandIcons() {
@@ -30,5 +68,3 @@ function toggleBrandIcons() {
     else
         $('#brandIcons').addClass('hidden')
 }
-
-console.log($('#navbarMain').height())
